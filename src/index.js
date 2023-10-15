3ab48ce495611c223126402560b39c0f8550b43c
mlilian611 <mdamyanova6@gmail.com>
2023-10-15 14:21:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// ReactDOM.render(<App />, document.getElementById("root"));
 const root = document.getElementById("root");
 
-const appRoot = ReactDOM.createRoot(root); // Create a root
+const appRoot = ReactDOM.createRoot(root);
 
 appRoot.render(<App />);
